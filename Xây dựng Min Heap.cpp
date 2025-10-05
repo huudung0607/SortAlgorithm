08b7573a8764cf4e40f54cf22e67a67c8f8d7481
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-10-05 20:23:50 +0700

    Create Xây dựng Min Heap.cpp

---
+++ Xây dựng Min Heap.cpp
@@ -0,0 +1,45 @@
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <cmath>
+#include <numeric>
+#include <climits>
+#include <iomanip>
+#include <string>
+#include <sstream>
+#include <queue>
+#include <stack>
+#include <unordered_map>
+using namespace std;
+void heapify(vector<int> &a, int n, int i) {
+	int smallest = i;
+	int left = 2 * i + 1;
+	int right = 2 * i + 2;
+	if (left < n && a[left] < a[smallest]) {
+		smallest = left;
+	}
+	if (right < n && a[right] < a[smallest]) {
+		smallest = right;
+	}
+	if (smallest != i) {
+		swap(a[i], a[smallest]);
+		heapify(a, n, smallest);
+	}	
+}
+void buildHeap(vector<int> &a, int n) {
+	for (int i = n/2 - 1; i >= 0; i--) {
+		heapify(a, n, i);
+	}
+}
+int main()
+{
+	vector<int> a = { 6, 10, 5, 7, 11, 8, 16, 4, 9, 14, 18, 21 };
+	buildHeap(a, 12);
+	for (int i : a) {
+		cout << i << " ";
+	}
+}
+
